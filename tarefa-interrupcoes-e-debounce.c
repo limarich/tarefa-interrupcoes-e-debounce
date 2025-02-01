125c9b835cf4d19c9157e660535009b28791fa5a
Richard <richard.esclima@gmail.com>
2025-02-01 09:55:39 -0300

    refactor: reorganiza o código separando os módulos de cada funcionalidade

--- tarefa-interrupcoes-e-debounce.c
+++ tarefa-interrupcoes-e-debounce.c
@@ -16,38 +16,63 @@ void gpio_irq_handler(uint gpio, uint32_t events)
     printf("Interrupção no GPIO %d, evento: %d\n", gpio, events);
 }
 
+void PIO_setup(PIO *pio, uint *sm);
+void button_setup(uint gpio);
+void clock_setup();
+void enable_interrupt();
+
 int main()
 {
-    bool ok;
     stdio_init_all();
+    PIO pio;
+    uint sm;
 
-    ok = set_sys_clock_khz(128000, false);
-
-    printf("iniciando a transmissão PIO");
-    if (ok)
-        printf("clock set to %ld\n", clock_get_hz(clk_sys));
-
+    // configura o clock
+    clock_setup();
     // configurações da PIO
-    PIO pio = pio0;
-    uint offset = pio_add_program(pio, &pio_matrix_program);
-    uint sm = pio_claim_unused_sm(pio, true);
-    pio_matrix_program_init(pio, sm, offset, LED_PIN);
+    PIO_setup(&pio, &sm);
+    // configuração dos botões esquerdo e direito
+    button_setup(button_a);
+    button_setup(button_b);
+    // configuração da interrupção nos dois botões
+    enable_interrupt();
 
-    // configuração do botão esquerdo
-    gpio_init(button_a);
-    gpio_set_dir(button_a, GPIO_IN);
-    gpio_pull_up(button_a); // habilitando pull_up
-    // configuração do botão direito
-    gpio_init(button_b);
-    gpio_set_dir(button_b, GPIO_IN);
-    gpio_pull_up(button_b); // habilitando pull_up
-
-    gpio_set_irq_enabled_with_callback(button_a, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
-    gpio_set_irq_enabled_with_callback(button_b, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
     // teste da matriz de leds
     test_matrix(pio, sm);
     draw_number(pio, sm);
     while (true)
     {
     }
+}
+
+void clock_setup()
+{
+    bool ok;
+    ok = set_sys_clock_khz(128000, false);
+
+    if (ok)
+        printf("clock set to %ld\n", clock_get_hz(clk_sys));
+}
+
+void button_setup(uint gpio)
+{
+    // configuração do botão esquerdo
+    gpio_init(gpio);
+    gpio_set_dir(gpio, GPIO_IN);
+    gpio_pull_up(gpio); // habilitando pull_up
+}
+
+void PIO_setup(PIO *pio, uint *sm)
+{
+    // configurações da PIO
+    *pio = pio0;
+    uint offset = pio_add_program(*pio, &pio_matrix_program);
+    *sm = pio_claim_unused_sm(*pio, true);
+    pio_matrix_program_init(*pio, *sm, offset, LED_PIN);
+}
+
+void enable_interrupt()
+{
+    gpio_set_irq_enabled_with_callback(button_a, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_set_irq_enabled_with_callback(button_b, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 }
